e641ae87478cd80d07125acfc92ae80361f5a863
Jonasgrove <jonasgrove@gmail.com>
2020-10-07 21:25:10 -0700

    all done

--- crop_data_slurm.sh
+++ crop_data_slurm.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #SBATCH --partition=bgmp        ### Partition (like a queue in PBS)
-#SBATCH --job-name=nyas_model      ### Job Name
-#SBATCH --output=nyas_model.out       ### File in which to store job output
-#SBATCH --error=nyas_model.er        ### File in which to store job error messages
+#SBATCH --job-name=nyas_model_v3      ### Job Name
+#SBATCH --output=nyas_model_v3.out       ### File in which to store job output
+#SBATCH --error=nyas_model_v3.er        ### File in which to store job error messages
 #SBATCH --time=0-10:00:00       ### Wall clock time limit in Days-HH:MM:SS
 #SBATCH --nodes=1               ### Number of nodes needed for the job
 #SBATCH --ntasks-per-node=1     ### Number of tasks to be launched per Node
 #SBATCH --account=bgmp          ### Account used for job submission
 #SBATCH --cpus-per-task=1       ##number of cpus (cores) per task
+#SBATCH --mem=50000
 
 conda activate bgmp_py37
 
-./nyas_model.py
+./nyas_model_94.py
---
+++ nyas_model_94.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import pandas as pd 
+import tensorflow as tf
+from sklearn import preprocessing
+import numpy as np
+
+all_data = pd.read_csv("prep_cereal_data", sep='\t', header=0)
+
+
+#turn assessment type and variety into dummy vars
+at_dums = pd.get_dummies(all_data["Assessment Type"])
+all_data = all_data.drop(columns="Assessment Type")
+all_data = pd.concat([all_data, at_dums], axis=1)
+
+var_dums = pd.get_dummies(all_data["Variety"])
+all_data = all_data.drop(columns="Variety")
+all_data = pd.concat([all_data, var_dums], axis=1)
+
+all_data = all_data.drop(columns="Site ID")
+all_data = all_data.dropna()
+all_data = all_data[all_data["Assessment Score"] != '*']
+
+#split features and target
+Y = all_data["Assessment Score"]
+X = all_data.drop(columns="Assessment Score")
+
+#scale features
+from sklearn.preprocessing import RobustScaler
+transformer = RobustScaler().fit(X)
+X = transformer.transform(X)
+
+Y = np.array(Y)
+Y[Y == ''] = 0.0
+Y = Y.astype(np.float)
+
+#make dense network model 
+import neural_net
+
+NeuralNet = neural_net.NeuralNet
+
+crop_score_model = NeuralNet(X, Y, 5, 259, "r", 500)
+crop_score_model.model.save
+
+crop_score_model.model.save_weights("crop_model_weights.tf")
+
+
+#check accuracy
+from sklearn.metrics import mean_squared_error
+
+y_pred = crop_score_model.test_result
+print(y_pred)
+
+accuracy = mean_squared_error(crop_score_model.y_test ,y_pred)
+print("Accuracy", accuracy)
+
